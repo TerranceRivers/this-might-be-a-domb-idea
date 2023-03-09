be9b87f0a4f609af78b0b693c37eec8b938f27d6
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-03-09 13:08:22 -0500

    new

---
+++ main.js
@@ -0,0 +1,28 @@
+let para = document.querySelector("p");
+para.style.color = "lightblue";
+
+let heading = document.querySelector("h1");
+heading.style.fontSize = "48";
+
+para.innerText =
+  " Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3")
+item3.innerText = " I say, 'Hi!"
+
+let img1 = document.querySelector("img")
+img1.src = "https://i.insider.com/60f860760729770012b91c62?width=1000&format=jpeg&auto=webp"
+img1.height = "300";
+
+let img2 = document.querySelector("#img2")
+img2.src = "https://i.insider.com/6324b8e7a6c3e80019669b4a?width=1136&format=jpeg"
+img2.height = "300"
+
+let item16 = document.querySelector("#item-16")
+let item = item16.className
+
+let lines = document.querySelector("#item-16")
+lines.innerText = " Won't get fooled again."
